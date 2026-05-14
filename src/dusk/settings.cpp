732fb18e322b5e12d04a63ef01e4041e1f2c88ea
Olivia!! <hakurou46@gmail.com>
2026-05-14 02:50:10 +0200

    Revert "Adds a new cheat to let you transform from the start of the game, without Midna or the Shadow Crystal"
    This reverts commit 51ed7367295a1cfc60be0adf2d16dcb84f2c0821.

--- src/dusk/settings.cpp
+++ src/dusk/settings.cpp
@@ -108,7 +108,6 @@ UserSettings g_userSettings = {
         .fastSpinner {"game.fastSpinner", false},
         .freeMagicArmor {"game.freeMagicArmor", false},
         .invincibleEnemies {"game.invincibleEnemies", false},
-        .transformWithoutShadowCrystal {"game.transformWithoutShadowCrystal", false},
 
         // Technical
         .restoreWiiGlitches {"game.restoreWiiGlitches", false},
@@ -192,7 +191,6 @@ void registerSettings() {
     // Game
     Register(g_userSettings.game.language);
     Register(g_userSettings.game.enableQuickTransform);
-    Register(g_userSettings.game.transformWithoutShadowCrystal);
     Register(g_userSettings.game.hideTvSettingsScreen);
     Register(g_userSettings.game.biggerWallets);
     Register(g_userSettings.game.noReturnRupees);
